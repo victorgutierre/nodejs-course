bf2c4251e19f1a50858e2e92edc50b825656a6a7
Victor Gutierre <victorsgutierre@gmail.com>
2016-05-21 11:00:56 -0300

    consumindo json

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,15 +1,22 @@
 'use strict';
 
 let MainController = {
-	home: function(request, response, next) {
-		response.send('Estou dentro do Controller');
-	},
-	create: function(request, response, next) {
-		response.send('Create blahhhh')
-	},
 	middleware: function(request, response, next) {
 		console.log('middleware');
 		next();
+	},
+	home: function(request, response, next) {
+		let json = {
+			name: 'Victor',
+			age: 19,
+			workAt: 'Beleza Na Web',
+			description: 'Front end Developer'
+		};
+		response.status(404);
+		response.send(json);
+	},
+	create: function(request, response, next) {
+		response.send('Create blahhhh')
 	}
 };
 
