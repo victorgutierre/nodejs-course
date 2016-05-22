362c462adaeb221b32560e1567807b3b341e7eb7
Victor Gutierre <victorsgutierre@gmail.com>
2016-05-22 11:55:01 -0300

    add: test in stormtroppers

---
+++ tests/stormtropper.test.js
@@ -0,0 +1,55 @@
+'use strict';
+
+let app = require('../app');
+let request = require('supertest')(app);
+let assert = require('assert');
+let debug = require('debug')('curso_nodejs:test:stormtroppers');
+
+describe('stormtropper crud', function(){
+	
+	it('GET /api/stormtroppers', function(done) {
+		request
+			.get('/api/stormtroppers')
+			.end(function(err, result) {
+				debug(result.body.length);
+				assert.equal(result.body.length, 4);
+				done();
+			});
+	});
+
+	it('GET /api/stormtroppers/:id', function(done) {
+		request
+			.get('api/stormtroppers/5740976f2902b4869ec96b84')
+			.end(function(err, result) {
+				debug(result.body);
+				done();
+		});
+	});
+
+	it('POST /api/stormtroppers', function(done) {
+		request
+			.post('/api/stormtroppers')
+			.send({'name':'ct-test', 'alias' : 'tester storm'})
+			.set({'Content-Type', 'application/x-www-form-urlencoded'})
+			.end(function(err, result) {
+				debug(result.body);
+				done();
+			});
+	});
+
+	it.only('PUT /api/stormtroppers/:id', function(done) {
+		request
+			.put('/api/stormtroppers/574099bd2902b4869ec96b85')
+			.send({ 'name': 'test PUT'})
+			.set({'Content-Type', 'application/x-www-form-urlencoded'})
+			.end(function(err, result) {
+				debug(result.body)
+				done();
+			});
+	});
+
+
+	it('DELETE /api/stormtroppers/:id', function() {
+
+	});
+});
