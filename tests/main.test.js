87ea997dba812cc2e9b9a7f490a500c7b03e21ad
Victor Gutierre <victorsgutierre@gmail.com>
2016-05-22 09:35:58 -0300

    add: test example

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,12 +1,16 @@
 'use strict';
 
-describre('main routes', function(){
+describe('main routes', function(){
 	it('GET / should respond 200',function() {
-
+		// throw new Error('erro'); - Simulando erro
 	});
 
 	it('GET /not-found should respond 404',function() {
 
 	});
-	
+
+	it('GET /api should respond blah',function() {
+
+	});
+
 });
