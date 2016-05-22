053cc4e3c7cd316740f63993b05ab95110cb9771
Victor Gutierre <victorsgutierre@gmail.com>
2016-05-22 14:05:16 -0300

    add: node escalar vertical

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,23 @@
 
 let router = require('express').Router();
 let MainController = require('../controllers/MainController');
+let pkg = require('../package.json');
+
 
 // Chama os objetos do MainController
 router.use('/', MainController.middleware);
 router.get('/', MainController.home);
 router.post('/', MainController.create);
+
+router.get('/health', function(request, response) {
+	response.json({
+		version: pkg.version,
+    pid: process.pid,
+    memory: process.memoryUsage(),
+    uptime: process.uptime(),
+    env: process.env.NODE_ENV
+	});
+})
 
 router.use('/api', require('./api'));
 
