216f44ed77fa746064c7a8ecc853d4a8157b4eb4
Victor Gutierre <victorsgutierre@gmail.com>
2016-05-22 09:39:08 -0300

    add: more example tests

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,15 +1,18 @@
 'use strict';
 
 describe('main routes', function(){
-	it('GET / should respond 200',function() {
-		// throw new Error('erro'); - Simulando erro
+	
+	// .skip or xit instead of it to skip a test
+	it.skip('GET / should respond 200',function() {
+		throw new Error('erro'); // - Simulando erro
 	});
 
 	it('GET /not-found should respond 404',function() {
 
 	});
 
-	it('GET /api should respond blah',function() {
+	// .only to test just one guy and focus on him
+	it.only('GET /api should respond blah',function() {
 
 	});
 
